17b0d0a9905152165d60f2f776abad5369b9f07a
Abtidon25 <112092486+Abtidon25@users.noreply.github.com>
2023-08-25 14:35:02 +0200

    Add files via upload

---
+++ oppgave1.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+int main() {
+    int x = 10;
+    int y = 4;
+    
+    printf("hei verden" );
+}
+#include<stdio.h>
+int main() {
+    int x = 15;
+    printf("%d",x);
+return 0;
+
+}
+#include<stdio.h>
+int main() {
+    // Dette er mitt favorittnummer
+    int x = 15;
+    printf("mitt favoritenummner er:%d", x);
+}
+
+#include<stdio.h>
+int main() {
+    // Disse er forskjellige datatyper.
+    int x = 15;
+    char minBokstaven = 'D';
+    printf ("Mitt nummer er %d og Min Bokstaven er %c", x, minBokstaven);
+    return 0;
+
+}
+#include<stdio.h>
+int main() {
+    //Endring av existrende vardi
+    int x = 15;
+     x = 10; 
+    printf("%d", x);
+   
+     
+
+}
+include<stdio.h>
+int main() {
+    //Endring av existrende vardi
+    int x = 15;
+     x = 10; 
+    printf("%d", x);
+   
+     
+
+}
+#include<stdio.h>
+int main() {
+     //addere to vardier
+    int x = 15;
+    int y = 10; 
+    int sum = x + y;
+    printf("%d", sum);
+   
+     
+
+}
+ 
+  
+
+
+
+
+
+
+
